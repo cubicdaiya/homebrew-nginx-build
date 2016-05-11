6a6cee6c6599d358c2ce397f921a84a3ab0839ac
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-05-12 02:35:04 +0900

    Revert "go get cubicdaiya/nginx-build"
    This reverts commit 5a6f927bd8ce4af29a72686a3b43668db532516e.

--- nginx-build.rb
+++ nginx-build.rb
@@ -15,7 +15,6 @@ class NginxBuild < Formula
     mkdir_p buildpath/'src/github.com/cubicdaiya/nginx-build'
     ln_s buildpath/'nginx-build', buildpath/'src/github.com/cubicdaiya/nginx-build/.'
     system 'go', 'get', 'github.com/go-ini/ini'
-    system 'go', 'get', 'github.com/cubicdaiya/nginx-build'
     system 'go', 'build', '-o', 'nginx-build'
     bin.install 'nginx-build'
   end
