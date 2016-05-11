8656150c7ace572ab4342890f2c40b266f1d0049
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-05-12 02:22:47 +0900

    bumped version to 0.9.3.

--- nginx-build.rb
+++ nginx-build.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-HOMEBREW_NGINX_BUILD_VERSION='0.9.1'
+HOMEBREW_NGINX_BUILD_VERSION='0.9.3'
 class NginxBuild < Formula
   homepage 'https://github.com/cubicdaiya/nginx-build'
   url 'https://github.com/cubicdaiya/nginx-build.git', :tag => "v#{HOMEBREW_NGINX_BUILD_VERSION}"
